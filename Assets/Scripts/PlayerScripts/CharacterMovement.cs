11fe25c2f9e9989d263e792b7f134d2ba2d07100
peterckim <peterckim96@gmail.com>
2018-08-11 19:15:31 -0400

    init

---
+++ Assets/Scripts/PlayerScripts/CharacterMovement.cs
@@ -0,0 +1,79 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+    private MovementMotor motor;
+
+    public float move_Magnitude = 0.5f;
+    public float speed = 0.7f;
+    public float speed_Move_WhileAttack = 0.1f;
+    public float speed_Attack = 1.5f;
+    public float turnSpeed = 10f;
+    public float speed_Jump = 20f;
+
+    private float speed_Move_Multiplier = 1f;
+
+    private Vector3 direction;
+
+    private Animator animator;
+    private Camera mainCamera;
+
+    private string PARAMETER_STATE = "State";
+
+    void Awake()
+    {
+        motor = GetComponent<MovementMotor>();
+        //animator = GetComponent<Animator>();
+    }
+
+    void Start () {
+        mainCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MovementAndJumping();
+	}
+
+    private Vector3 MoveDirection {
+        get { return direction; }
+
+        set {
+            direction = value * speed_Move_Multiplier;
+
+            if (direction.magnitude > 0.1f) {
+                var newRotation = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * turnSpeed);
+            }
+
+            direction *= speed * (Vector3.Dot(transform.forward, direction) + 1f) * 5f;   
+            motor.Move(direction);
+        }
+    }
+
+    void Moving(Vector3 dir, float mult) {
+        speed_Move_Multiplier = 1 * mult;
+        MoveDirection = dir;
+    }
+
+    void Jump() {
+        motor.Jump(speed_Jump);
+    }
+
+    void MovementAndJumping() {
+        Vector3 moveInput = Vector3.zero;
+        Vector3 forward = Quaternion.AngleAxis(-90, Vector3.up) * mainCamera.transform.right;
+
+        moveInput += forward * Input.GetAxis("Vertical");
+        moveInput += mainCamera.transform.right * Input.GetAxis("Horizontal");
+
+        moveInput.Normalize();
+        Moving(moveInput.normalized, 1f);
+
+        if (Input.GetKey (KeyCode.Space)) {
+            Jump();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/MovementMotor.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementMotor : MonoBehaviour {
+
+    public float gravityMultiplier = 1f;
+    public float lerpTime = 10f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private Vector3 targetDirection = Vector3.zero;
+
+    private float fallVelocity = 0f;
+
+    [HideInInspector]
+    public CharacterController characterController;
+
+    public float distanceToGround = 0.1f;
+
+    private bool isGrounded;
+
+    private Collider myCollider;
+
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        myCollider = GetComponent<Collider>();
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        distanceToGround = myCollider.bounds.extents.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        isGrounded = OnGroundCheck();
+
+        moveDirection = Vector3.Lerp(moveDirection, targetDirection, Time.deltaTime * lerpTime);
+        moveDirection.y = fallVelocity;
+
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        if (!isGrounded) {
+            fallVelocity -= 90f;
+        }
+	}
+
+    public bool OnGroundCheck()
+    {
+        RaycastHit hit;
+
+        if (characterController.isGrounded)
+        {
+            return true;
+        }
+        if (Physics.Raycast(myCollider.bounds.center, -Vector3.up, out hit, distanceToGround + -.1f))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void Move(Vector3 dir) {
+        targetDirection = dir;
+    }
+
+    public void Stop() {
+        moveDirection = Vector3.zero;
+        targetDirection = Vector3.zero;
+    }
+
+    public void Jump(float jumpSpeed) {
+        if (isGrounded) {
+            fallVelocity = jumpSpeed;
+        }
+    }
+}
